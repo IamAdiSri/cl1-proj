ac1b5a07d678b7bfca3bf830e4193264a1f80538
pranavgoel25 <pranav.goel@research.iiit.ac.in>
2016-11-05 19:40:58 +0530

    Add parent name in place of Parent Id

--- modify-data.py
+++ modify-data.py
@@ -6,7 +6,6 @@ arr = []
 def BIO_Tag(templine, already):
     pipe_sep = templine[5].split('|')
     chunk = pipe_sep[7].split('-')
-    # print(chunk)
     if chunk[1] not in already:
         already.append(chunk[1])
         temp = chunk[1]
@@ -17,7 +16,6 @@ def BIO_Tag(templine, already):
         temp += '_I'
         chunk[1] = temp
     temp = chunk[0] + '-' + chunk[1]
-    # print(temp)
     pipe_sep[7] = temp
     temp_append = pipe_sep[0]
     for i in range(1, 11):
@@ -29,7 +27,7 @@ def add_parent_name(arr):
     for i in range(0, Wcount):
         temp = arr[i][6]
         if temp == '0':
-            arr[i][6]='root'
+            arr[i][6] = 'root'
         else:
             for j in range(0, Wcount):
                 temp2 = arr[j][0]
@@ -44,16 +42,11 @@ with open('hdtb_development_wx.conll') as f:
             for i in range(0, Wcount):
                 print(arr[i])
             print('\n')
-            # print("BLANK LINE\n")
             already = []
             Wcount = 0
             arr = []
         else:  # Normal Line
-            # print(line)
             templine = line.split('\t')
-            # print(templine)
             BIO_Tag(templine, already)
-            # print(templine)
             arr.append(templine)
             Wcount += 1
-            # print("LINE ENDS\n")
