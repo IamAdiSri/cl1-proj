e5ec63e2b34dab08e84b1b65e4e2efe3b03f6e3f
pranavgoel25 <pranav.goel@research.iiit.ac.in>
2016-11-12 14:06:22 +0530

    Final dump for Hindi ready

--- dump.py
+++ dump.py
@@ -1,33 +1,38 @@
 from __future__ import print_function
-import mysql.connector
+import mysql.connector as connector
 
 already = []
 Wcount = 0
-Scount = 1
+Scount = 15895
 arr = []
 
-cnx = mysql.connector.connect(user='root', password='gotham', host='localhost', database='test_cl')
+cnx = connector.connect(user='root', password='gotham', host='localhost', database='test_cl')
 cursor = cnx.cursor()
 print("Connected!")
 
 
-def dump(arr):
+def dump(array):
     add_data = (
         "INSERT INTO hdtb (Sid, Wid, form, lemma, cpos, pos, category, gender, number, person, cas, vibh, tam, chunkid, chunktype, stype, voicetype, parent, drel)"
         "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     )
-    arr.remove(arr[19])
-    arr.remove(arr[19])
-    data_test = tuple(arr)
+    array.remove(array[19])
+    array.remove(array[19])
+    data_test = tuple(array)
     cursor.execute(add_data, data_test, False)
     cnx.commit()
 
 
-def BIO_Tag(templine, already):
-    pipe_sep = templine[5].split('|')
+def stringify(array):
+    for j in range(0, 19):
+        array[j] = str(array[j])
+
+
+def BIO_Tag(tline, already_words):
+    pipe_sep = tline[5].split('|')
     chunk = pipe_sep[7].split('-')
-    if chunk[1] not in already:
-        already.append(chunk[1])
+    if chunk[1] not in already_words:
+        already_words.append(chunk[1])
         temp = chunk[1]
         temp += '_B'
         chunk[1] = temp
@@ -38,29 +43,29 @@ def BIO_Tag(templine, already):
     temp = chunk[0] + '-' + chunk[1]
     pipe_sep[7] = temp
     temp_append = pipe_sep[0]
-    for i in range(1, 11):
-        temp_append += '|' + pipe_sep[i]
-    templine[5] = temp_append
+    for q in range(1, 11):
+        temp_append += '|' + pipe_sep[q]
+    tline[5] = temp_append
 
 
-def add_parent_name(arr):
-    for i in range(0, Wcount):
-        temp = arr[i][7]
+def add_parent_name(array):
+    for q in range(0, Wcount):
+        temp = array[q][7]
         if temp == '0':
-            arr[i][7] = 'root'
+            array[q][7] = 'root'
         else:
             for j in range(0, Wcount):
-                temp2 = arr[j][1]
+                temp2 = array[j][1]
                 if temp2 == temp:
-                    arr[i][7] = arr[j][2]
+                    array[q][7] = array[j][2]
 
 
-def final_form_arr(arr):
-    temp1 = arr[7]
-    temp2 = arr[8]
-    temp3 = arr[9]
-    temp4 = arr[10]
-    features_array = arr[6]
+def final_form_arr(array):
+    temp1 = array[7]
+    temp2 = array[8]
+    temp3 = array[9]
+    temp4 = array[10]
+    features_array = array[6]
     features_split = features_array.split('|')
     num_features = len(features_split)
     index = 6
@@ -70,31 +75,34 @@ def final_form_arr(arr):
         if final == '':
             final = '-'
         if index > 10:
-            arr.insert(index, final)
+            array.insert(index, final)
         else:
-            arr[index] = final
+            array[index] = final
         index += 1
-    arr.insert(index, temp1)
+    array.insert(index, temp1)
     index += 1
-    arr.insert(index, temp2)
+    array.insert(index, temp2)
     index += 1
-    arr.insert(index, temp3)
+    array.insert(index, temp3)
     index += 1
-    arr.insert(index, temp4)
+    array.insert(index, temp4)
 
 
-with open('test.txt') as f:
+with open('hdtb_development_wx.conll') as f:
     for line in f:
         if line in ['\n', '\r\n']:  # Blank Line
-            #add_parent_name(arr)
+            # add_parent_name(arr)
             for i in range(0, Wcount):
                 final_form_arr(arr[i])
+                stringify(arr[i])
                 dump(arr[i])
-            for i in range(0, Wcount):
-                print(arr[i])
-            print('\n')
+            # for i in range(0, Wcount):
+            #     print(arr[i])
+            # print('\n')
             already = []
             Wcount = 0
+            print(Scount)
+            # print('\n')
             Scount += 1
             arr = []
         else:  # Normal Line
